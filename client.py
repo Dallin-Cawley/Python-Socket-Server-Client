149ec00fba5a2fd9867f717d850b4253553035a5
Dallin Cawley <dallin.cawley@gmail.com>
2020-04-18 20:03:23 -0600

    The list of directories and files on server is now retrievable

--- client.py
+++ client.py
@@ -13,12 +13,16 @@ def main():
     print(client_socket.recv(1024).decode('UTF-8'), '\n')
 
     # Send some information to the host.
-    user_input = raw_input('What would you like to do?\n\t1. Send File\n\t2. Send Message\n\t3. Quit\nInput: ').lower()
+    user_input = raw_input('What would you like to do?\n'
+                           '\t1. Send File\n'
+                           '\t2. Send Message\n'
+                           '\t3. Quit\n'
+                           '\t4. View Files\n'
+                           'Input: ').lower()
 
     while True:
 
         if user_input == 'quit':
-
             body = {
                 'header': 'quit'
             }
@@ -45,6 +49,12 @@ def main():
                 message = client_socket.recv(1024).decode('UTF-8')
                 print(message)
 
+        elif user_input == 'view files':
+            handle_file_view(client_socket=client_socket, requested_directory=os.path.join("C:\\", "Users", "lette",
+                                                                                           "PycharmProjects",
+                                                                                           "Python-Socket-Server-Client",
+                                                                                           "files-received"))
+
         user_input = raw_input('\nWhat would you like to do?\n\t1. Send File\n\t2. Send Message\n\t3. Quit\nInput: ')
 
     client_socket.close()
@@ -59,6 +69,7 @@ def send_file(client_socket, file_name):
             'file_type': os.path.splitext(file_path)[1],
             'file_name': os.path.basename(file_path),
             'file_size': file_size,
+            'directory': 'files-received'
         }
 
         file = open(file_path, 'rb')
@@ -73,6 +84,17 @@ def send_file(client_socket, file_name):
     except FileNotFoundError:
         print('File not found. Try again.')
         pass
+
+
+def handle_file_view(client_socket, requested_directory):
+    body = {
+        'header': 'ls',
+        'current_directory': requested_directory
+    }
+
+    client_socket.sendall(json.dumps(body).encode('UTF-8'))
+    current_directory_list = client_socket.recv(1024).decode('UTF-8')
+    print(current_directory_list)
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 from _thread import *
 from pip._vendor.distlib.compat import raw_input
 import json
+import os
 
 
 def main():
@@ -40,24 +41,65 @@ def handle_client_connection(client_connection):
     while True:
         data = client_connection.recv(1024)
         request_body = json.loads(data)
-
-        if request_body.get('header') == 'quit':
+        header = request_body.get('header')
+        # When the Client wishes to disconnect
+        if header == 'quit':
             response_data = 'Connection Terminating'
             client_connection.send(response_data.encode('UTF8'))
             break
 
-        if request_body.get('header') == 'file':
-            file = open('files-recieved/' + request_body.get('file_name'), 'wb')
-            sent_file = client_connection.recv(request_body.get('file_size'))
-            file.write(sent_file)
-            file.close()
+        # If the client wishes to manipulate files
+        if header == 'file':
+            handle_file(request_body=request_body, client_connection=client_connection)
             client_connection.send('File saved'.encode('UTF-8'))
 
-        elif request_body.get('header') == 'message':
+        # If the client wishes to just send a message
+        elif header == 'message':
             client_connection.send(('You sent: ' + request_body.get('message')).encode('UTF-8'))
             client_connection.send('end'.encode('UTF-8'))
 
+        # If the client wants the list of the current directory
+        elif header == 'ls':
+            client_connection.send(get_current_directory_names(request_body=request_body))
+
     client_connection.close()
+
+
+def get_current_directory_names(request_body):
+    dir_file = os.walk(request_body.get('current_directory'))
+    dict_of_dict_of_files = {}
+    i = 0
+    for root, directory, files in dir_file:
+        dict_of_files = {
+            'root': root,
+            'directories': directory,
+            'file_names': files,
+        }
+        dict_of_dict_of_files.update({i: dict_of_files})
+        i += 1
+
+    return json.dumps(dict_of_dict_of_files).encode('UTF-8')
+
+
+def handle_file(request_body, client_connection):
+    # Create file path
+    directory = os.path.join("C:\\", "Users", "lette", "PycharmProjects", "Python-Socket-Server-Client",
+                             request_body.get('directory'))
+    print('Directory: ', directory)
+
+    # If the requested directory doesn't exist, create it.
+    if not os.path.exists(directory):
+        print('Directory: ', directory)
+        os.mkdir(directory)
+
+    # Get the file from the client
+    sent_file = client_connection.recv(request_body.get('file_size'))
+    file_name = os.path.join(directory, request_body.get('file_name'))
+
+    # Write the file to specified location
+    file = open(file_name, 'wb')
+    file.write(sent_file)
+    file.close()
 
 
 if __name__ == '__main__':
